9e7c785c390140aa0ae0a485c11d885a4e8b1935
Nick Chapman <nickpchapman@gmail.com>
2017-12-09 11:24:57 -0800

    added script for auto compiling, added landing page placeholder

---
+++ Client/src/components/landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Landing extends React.Component {
+  render () {
+    return (
+      <div>
+        <div>Search form goes here</div>
+        <div>Landing Image wall goes here</div>
+      </div>
+    )
+  }
+}
+
+export default Landing;
--- Client/src/index.js
+++ Client/src/index.js
@@ -1,13 +1,14 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Landing from './components/landing.jsx'
 
 class App extends React.Component {
   render () {
     return (
       <div>
-        <p id="test"> Hello React project</p>
+        <p id="test"> Hello React project!</p>
         <h1>To the moon!</h1>
+        <Landing />
       </div>
     )
   }
