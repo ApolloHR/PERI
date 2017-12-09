048f02fa32d2d8c9fc557fd780d828b99dd4d07e
Raymond Huynh <huynh.r.k@gmail.com>
2017-12-09 14:40:25 -0500

    deployment with mongodb

--- Db/index.js
+++ Db/index.js
@@ -1,14 +1,46 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect('mongodb://localhost/peri' ||
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERVER}`);
+
 const User = mongoose.Schema;
 const Trips = mongoose.Schema;
 const Spot = mongoose.Schema;
+const Forked = mongoose.Schema;
 
 const userSchema = new User({
-  name: String,
+  username: {
+    type: String,
+    unique: true
+  },
   sessionID: String,
 })
 
 const tripsSchema = new Trips({
-
+  username: String,
+  tripName: String,
+  destination: String,
+  description: String,
+  spots: [{spotID: Number}] //save the spots ID in here
 })
+
+const spotSchema = new Spot({
+  tripID: String,
+  spotName: String,
+  destination: String,
+  description: String,
+  long: Number,
+  lat: Number,
+  elevation: Number,
+  photo: String
+})
+
+const forkedSchema = new Forked({
+  userID: String,
+  destination: String,
+  spots: [{spotID: Number}]
+})
+
+
 
