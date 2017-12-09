132a855c212564362ade9fbe4f431f59cc9617e1
Nick Chapman <nickpchapman@gmail.com>
2017-12-09 11:40:54 -0800

    created combine reducer file, updated webpack script

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Landing from './components/landing.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import Landing from "./components/landing.jsx"
 
 class App extends React.Component {
   render () {
