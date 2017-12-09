0acb3cbfd9bb28bafb05620b27b02d2723944d25
Nick Chapman <nickpchapman@gmail.com>
2017-12-09 12:26:08 -0800

    added fetchTrips action with dummy data

---
+++ Client/src/actions/tripsActions.js
@@ -0,0 +1,18 @@
+export function fetchTrips() {
+  return [{
+    username: 'nick',
+    tripName: 'London 2017',
+    destination: 'London',
+    description: 'Lads trip to UK 2017',
+    thumbnail: 'https://media.timeout.com/images/103042848/image.jpg',
+    spots: [{spotID: 1}]
+  },
+  {
+    username: 'larry',
+    tripName: 'London 2017',
+    destination: 'London',
+    description: 'Lads trip to UK 2017',
+    thumbnail: 'http://knowledge.wharton.upenn.edu/wp-content/uploads/2016/03/rio.jpg',
+    spots: [{spotID: 2}]
+  }]
+}
