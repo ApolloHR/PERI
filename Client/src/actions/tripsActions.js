335169ab519ad74381f3165a88142e26f23a3c28
Nick Chapman <nickpchapman@gmail.com>
2017-12-09 12:30:43 -0800

    Refactored fetch trips action to be in correct action object format

--- Client/src/actions/tripsActions.js
+++ Client/src/actions/tripsActions.js
@@ -1,5 +1,5 @@
 export function fetchTrips() {
-  return [{
+  return {type: "FETCH_TRIPS_SUCCESS", payload:[{
     username: 'nick',
     tripName: 'London 2017',
     destination: 'London',
@@ -14,5 +14,5 @@ export function fetchTrips() {
     description: 'Lads trip to UK 2017',
     thumbnail: 'http://knowledge.wharton.upenn.edu/wp-content/uploads/2016/03/rio.jpg',
     spots: [{spotID: 2}]
-  }]
+  }]}
 }
