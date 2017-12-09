5185a3afa99010673cc9989bdc039c14c444438c
Nick Chapman <nickpchapman@gmail.com>
2017-12-09 10:55:10 -0800

    added new folders for flux and removed ES6 from  react DOM import for clarity

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,5 +1,6 @@
+
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render () {
@@ -12,4 +13,4 @@ class App extends React.Component {
   }
 }
 
-render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
