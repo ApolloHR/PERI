5f34e2a10aca809e5400e468247defc1e5a96fcd
Benji <thebkrcanbefound@gmail.com>
2017-12-09 10:40:13 -0800

    working on server and sessions, rebasing

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,30 @@ const path = require('path');
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, '../Client/dst')))
+const session = require('express-session');
+const db = require('../Db/index.js')
 
+
+
+app.post('/signup', (req, res) => {
+
+})
+
+app.post('/login', (req, res) => {
+
+})
+
+app.get('/getTrips', (req, res) => {
+
+})
+
+app.post('/trip', (req, res) => {
+
+})
+
+
+
+
+
+app.use(express.static(path.join(__dirname, '../Client/dst')))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
