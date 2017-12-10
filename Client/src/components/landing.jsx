a8389b39ab5d41c7d7fb0a2941fb3321f97f01fe
Nick Chapman <nickpchapman@gmail.com>
2017-12-09 16:56:55 -0800

    changed start script to 'webpack' instead of node path. Added oneTrip.jsx to render each trip when page loads

--- Client/src/components/landing.jsx
+++ Client/src/components/landing.jsx
@@ -1,19 +1,26 @@
 import React from "react"
 import { connect } from "react-redux"
+import OneTrip from "./oneTrip.jsx"
+import { fetchTrips } from "../actions/tripsActions"
 
 @connect((store) => {
   return {
     trips: store.trips
   }
-}, )
+})
 class Landing extends React.Component {
+
+  componentDidMount() {
+    this.props.dispatch(fetchTrips())
+  }
 
   render () {
     return (
       <div>
         <div>Search form goes here</div>
-        <div>Landing Image wall goes here</div>
-        <div>{this.props.trips.test}</div>
+        <div>{this.props.trips.allTrips.map(tripObj => (
+             <OneTrip trip={tripObj} />
+        ))}</div>
       </div>
     )
   }
--- Client/src/reducers/trips.js
+++ Client/src/reducers/trips.js
@@ -1,7 +1,6 @@
 export default function reducer(state={
   allTrips: [],
   loggedIn: false,
-  test: 'hello world',
 }, action) {
   switch (action.type) {
     case "FETCH_TRIPS_SUCCESS": {
