d9bdf9e4ff8cee7cf9cd163c41f14826559e5fe8
Nick Chapman <nickpchapman@gmail.com>
2017-12-11 14:40:44 -0800

    Converted router to routes. Updated cart, buildTrip, and Nav to basic react components that work with the hash router

--- Client/src/components/nav.jsx
+++ Client/src/components/nav.jsx
@@ -1,20 +1,14 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
 
-let Nav = () => (
+let Nav = (props) => (
 
 <div>
-  <ul className="nav">
-    <li>
-      <NavLink to="/landing" activeClassName="active">Home</NavLink>
-    </li>
-    <li>
-      <NavLink to="/buildTrip" activeClassName="active">Build Trip</NavLink>
-    </li>
-    <li>
-      <NavLink to="/cart" activeClassName="active">Cart</NavLink>
-    </li>
-  </ul>
+  <nav className="nav">
+    <NavLink to="/" activeClassName="active"> Home </NavLink>
+    <NavLink to="/buildTrip" activeClassName="active"> Build Trip </NavLink>
+    <NavLink to="/cart" activeClassName="active"> Cart </NavLink>
+  </nav>
 </div>
 
   )
--- Client/src/components/routes.jsx
+++ Client/src/components/routes.jsx
@@ -1,17 +1,21 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Link, HashRouter } from "react-router-dom"
-import Nav from "./nav"
+import Nav from "./nav.jsx"
+import landing from "./landing.jsx"
+import buildTrip from "./buildTrip.jsx"
+import cart from "./cart.jsx"
 
-let Router = () => (
+let Routes = (props) => (
 
 <HashRouter>
   <div>
     <Nav />
-    <Route exact path="/" component={Landing}/>
+    <Route exact path="/" component={landing}/>
     <Route path="/buildTrip" component={buildTrip}/>
     <Route path="/cart" component={cart}/>
   </div>
 </HashRouter>
-  )
 
+)
 
+export default Routes;
