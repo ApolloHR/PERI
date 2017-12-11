9359fdf77c0a1e5e2db66c3bc69f0db3c8531264
Benji <thebkrcanbefound@gmail.com>
2017-12-11 12:39:24 -0800

    login button working as -a- tag but not button

---
+++ Client/src/components/login.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+
+@connect((store) => {
+  return {
+    login: store.login
+  }
+}, )
+
+class Login extends React.Component {
+
+
+  render () {
+    return (
+      <div>
+        <div>Build Your Own Adventure:</div>
+        <div>Login</div>
+        <form>
+          <a type="submit" href="/auth/google">Goooooogle Login</a>
+        </form>
+      </div>
+    )
+  }
+  // <div>{this.props.login.google}</div>
+}
+
+export default Login;
