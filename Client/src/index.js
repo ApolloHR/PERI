76aba2d8ca78eae2e3920516bfa0fba4ada2986c
Phillip Jun <29644671+waffiltime@users.noreply.github.com>
2017-12-11 20:01:13 -0800

    add basic redux routes for upload trip form, move uploader to buildtrip route

--- Client/src/index.js
+++ Client/src/index.js
@@ -4,36 +4,16 @@ import { Provider } from "react-redux"
 import store from "./store.js"
 import Routes from "./components/routes.jsx"
 import { BrowserRouter } from 'react-router-dom'
-import { CloudinaryContext, Transformation, Image } from 'cloudinary-react';
 class App extends React.Component {
 
-  uploadWidget() {
-    let _this = this;
-    window.cloudinary.openUploadWidget({ cloud_name: 'apollohr', cropping: 'server', theme: 'white', stylesheet: '#cloudinary-overlay { background-color: transparent; }', cropping_show_back_button: true, cropping_aspect_ratio: 1, sources: ['local', 'url', 'camera', 'google_photos', 'facebook', 'instagram'], show_powered_by: false, upload_preset: 'uploadperi', tags:['users', 'content']},
-    function(error, result) {
-      console.log(result);
-    });
-  }
+  
 
   render () {
     return (
       <div>
         <p id="test"> Hello React project!</p>
-        <h1>To the moon!</h1>
-        <div id="uploaded" className="upload">
-          <button onClick={this.uploadWidget.bind(this)} className="upload-button">
-            Add Image
-          </button>
-        </div>
-          <Routes />
-        <div>
-          <Image cloudName="apolloHR" publicId="public_id" width="300" crop="scale"/>
-          {/* <CloudinaryContext cloudName="apolloHR">
-            <Image publicId="user_uploads">
-              <Transformation width="200" crop="scale" angle="10"/>
-            </Image>
-          </CloudinaryContext> */}
-        </div>
+        <h1>To the moon!</h1>        
+          <Routes />        
       </div>
     )
   }
