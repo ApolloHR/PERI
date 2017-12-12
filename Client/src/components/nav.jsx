046c246e4df8d97530e8e674ce3fb4c07533f86c
Nick Chapman <nickpchapman@gmail.com>
2017-12-11 16:13:45 -0800

    added logo placeholder for navbar

--- Client/src/components/nav.jsx
+++ Client/src/components/nav.jsx
@@ -6,6 +6,7 @@ let Nav = (props) => (
 
 <div>
   <nav className="nav">
+  <span>PERI LOGO HERE</span>
     <NavLink to="/" activeClassName="active"> Home </NavLink>
     <NavLink to="/buildTrip" activeClassName="active"> Build Trip </NavLink>
     <NavLink to="/cart" activeClassName="active"> Cart </NavLink>
