1fd15f6cce4a290ee59b9bfa4effe7a037955554
Benji <thebkrcanbefound@gmail.com>
2017-12-13 15:34:31 -0800

    improves UX for Spots, more work to be done

--- Client/src/components/oneSpot.jsx
+++ Client/src/components/oneSpot.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {CardTitle, Card} from 'react-materialize';
 import axios from 'axios';
 
 class OneSpot extends React.Component {
@@ -30,11 +29,29 @@ class OneSpot extends React.Component {
       <div>
         {this.state.spots.map((spot) => {
           return (
-            <div>
-              <Card header={<CardTitle reveal image={spot.photo} waves='light'/>}
-                title={spot.spotName}reveal={<p>{spot.description}</p>}>
-                <p><a href="#">see map</a></p>
-              </Card>
+            <div class="box">
+              <div class="card-image">
+                <figure class="image is-4by5">
+                  <img src={spot.photo} alt="Placeholder image" />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src={spot.photo} alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">{spot.spotName}</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                  {spot.description}
+                  <br/>
+                </div>
+              </div>
             </div>
           );
         })}
