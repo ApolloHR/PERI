d475db1cc3a9343d4fba87545ff2dac14fa37e54
Benji <thebkrcanbefound@gmail.com>
2017-12-12 17:27:31 -0800

    added the oneSpot component

--- Client/src/components/allTripInfo.jsx
+++ Client/src/components/allTripInfo.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import OneSpot from "./oneSpot.jsx";
 
 
 let AllTripInfo = (props) => (
   <div>
+    <OneSpot trip={props.location.query.tripName}/>
     <div>Whole Trip Viewer</div>
     <div>{console.log(props)}</div>
     <div>{props.location.query.tripName}</div>
