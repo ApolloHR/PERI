e7fff03b47bcd7d2027931883756702976d14947
Benji <thebkrcanbefound@gmail.com>
2017-12-13 17:45:31 -0800

    Improves UX and overal design of AllTripInfo and OneSpot

--- Client/src/components/allTripInfo.jsx
+++ Client/src/components/allTripInfo.jsx
@@ -4,10 +4,23 @@ import OneSpot from './oneSpot.jsx';
 
 let AllTripInfo = (props) => (
   <div>
-    <Card className='large' header={<CardTitle image={props.location.query.thumbnail}>{props.location.query.tripName}</CardTitle>} actions={[<a href='#'>Find Another Adventure</a>]}>{props.location.query.description}
-    </Card>
+    <div className="card">
+      <div className="">
+        <img src={props.location.query.thumbnail} alt="Placeholder image"/>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{props.location.query.tripName}</p>
+            <br></br>
+            <p className="subtitle is-6">{props.location.query.description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
     <h3 class="title is-3">All Spots on This Trip:</h3>
     <OneSpot trip={props}/>
+
   </div>
 );
 
