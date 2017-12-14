50e6cb057f4441b51ff5b99918bd358b81c8e2d4
Raymond Huynh <huynh.r.k@gmail.com>
2017-12-13 20:44:31 -0500

    Delete the pink nav bar

--- Client/src/components/nav.jsx
+++ Client/src/components/nav.jsx
@@ -5,8 +5,8 @@ import Login from './login.jsx';
 let Nav = (props) => (
 
   <div>
-    <nav className="nav">
-      <span>PERI LOGO HERE</span>
+    <nav className="">
+      <span>PERI</span>
       <NavLink to="/" activeClassName="active"> Home </NavLink>
       <NavLink to="/buildTrip" activeClassName="active"> Build Trip </NavLink>
       <NavLink to="/cart" activeClassName="active"> Cart </NavLink>
