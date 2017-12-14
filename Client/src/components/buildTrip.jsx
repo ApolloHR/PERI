7984c204e49c1904b8b1fe99fd97e2e7e74e9700
Phillip Jun <29644671+waffiltime@users.noreply.github.com>
2017-12-13 16:06:24 -0800

    Create redux ADRV for complete trip, format image info array

--- Client/src/components/buildTrip.jsx
+++ Client/src/components/buildTrip.jsx
@@ -1,24 +1,40 @@
 import React from "react";
-import { CloudinaryContext, Transformation, Image } from 'cloudinary-react';
-import { uploadTrip } from '../actions/uploadActions.js';
+import { render } from "react-dom";
 import { connect } from "react-redux";
+// import { CloudinaryContext, Transformation, Image } from "cloudinary-react";
+import { uploadTrip } from "../actions/uploadActions.js";
+import { cloudinaryAction } from "../actions/cloudinary.js";
+import { fetchTrips } from "../actions/tripsActions";
 
 @connect ((store) => {
   return {
-    test: store.trips.uploadTrip
-  };
+    test: store.trips.uploadTrip,
+    cloudinaryGallery: store.cloudinary
+  }
 })
 
 class BuildTrip extends React.Component {
 
   uploadWidget() {
     let _this = this;
-    window.cloudinary.openUploadWidget({ cloud_name: 'peri', cropping: 'server', theme: 'white', cropping_show_back_button: true, cropping_aspect_ratio: 1, sources: ['local', 'url', 'camera', 'google_photos', 'facebook', 'instagram'], show_powered_by: false, upload_preset: 'uploadperi', tags:['users']},
+    window.cloudinary.openUploadWidget({ cloud_name: "peri", theme: "white", cropping: 'server', cropping_show_back_button: true, cropping_aspect_ratio: 1, sources: ["local", "url", "camera", "google_photos", "facebook", "instagram"], show_powered_by: false, upload_preset: "uploadperi", tags:["users"]},
     function(error, result) {
-      console.log(result);
+      if(error) {
+        console.log("upload widget error =", error);
+      }
+      console.log('uploadwidget result =', result);
+      let res = result.map((e) => {
+        return e.url;
+      })
+      _this.props.dispatch(cloudinaryAction(res));
     });
+    console.log('this.props.cloudinaryGallery =', this.props.cloudinaryGallery);
   }
 
+  saveData() {
+    //assemble data object
+    //send to trip previewer/post to server
+  }
 
   handleChange(e) {
     this.props.dispatch(uploadTrip(e.target.value));
