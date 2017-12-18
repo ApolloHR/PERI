71f76024fffa274334cd0c5afcf0208de524543a
Nick Chapman <nickpchapman@gmail.com>
2017-12-18 12:12:49 -0800

    Add snapshot tests for nav component

--- __tests__/__snapshots__/nav.test.js
+++ __tests__/__snapshots__/nav.test.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+// import { shallow } from 'enzyme';
 import Nav from '../../client/src/components/nav.jsx';
 import { MemoryRouter } from 'react-router';
 import { Link } from 'react-router-dom';
 
 
+describe('>>>N A V --- Test nav component', () => {
 
-it('renders correctly', () => {
+  it('renders home link', () => {
+  const tree = renderer.create(
+    <MemoryRouter>
+      <Link to="/" class="has-text-grey-dark"activeClassName="active" > Home </Link>
+    </MemoryRouter>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+  });
+
+  it('renders Build Trip link', () => {
   const tree = renderer
     .create(<MemoryRouter>
-      <Link to="/" class="has-text-grey-dark"activeClassName="active" > Home </Link>
+      <Link to="/buildTrip" class="has-text-grey-dark"activeClassName="active" > Build Trip </Link>
     </MemoryRouter>)
     .toJSON();
   expect(tree).toMatchSnapshot();
-});
+  });
 
+  it('renders Cart link', () => {
+  const tree = renderer
+    .create(<MemoryRouter>
+      <Link to="/cart" class="has-text-grey-dark"activeClassName="active" > Cart </Link>
+    </MemoryRouter>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+  });
+
+  it('renders G-login link', () => {
+  const tree = renderer
+    .create(<MemoryRouter>
+      <Link to="/auth/google" class="has-text-grey-dark"activeClassName="active" > G-login </Link>
+    </MemoryRouter>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+  });
+
+  it('renders FB-Login link', () => {
+  const tree = renderer
+    .create(<MemoryRouter>
+      <Link to="/auth/facebook" class="has-text-grey-dark"activeClassName="active" > FB-login </Link>
+    </MemoryRouter>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+  });
+});
