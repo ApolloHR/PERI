01901f260ab3a3d92a6add3b3f59d86a7f602bc7
Raymond Huynh <huynh.r.k@gmail.com>
2017-12-18 11:59:16 -0500

    Add reducers and action test

---
+++ __tests__/__snapshots__/reducersTest/reducers.test.js
@@ -0,0 +1,15 @@
+import reducer from '../../../Client/src/reducers/trips.js';
+
+describe('>>>R E D U C E R --- Test trips reducers', () => {
+  it('+++ reducer for trips', () => {
+    let state = { allTrips: [], loggedIn: false };
+    state = reducer(state, {
+      type: 'FETCH_TRIPS_SUCCESS',
+      payload: ['New Zealand', 'Australia', 'South Korea', 'Bali']
+    });
+    expect(state).toEqual({
+      allTrips: ['New Zealand', 'Australia', 'South Korea', 'Bali'],
+      loggedIn: false
+    });
+  });
+});
