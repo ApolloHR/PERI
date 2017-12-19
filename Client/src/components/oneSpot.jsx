f44fcd3ce63ede8ae2b16c90e2a6d2bc92338a88
Phillip Jun <29644671+waffiltime@users.noreply.github.com>
2017-12-18 20:20:30 -0800

    Created cart action/reducer, addtocart button, route, and store

--- Client/src/components/oneSpot.jsx
+++ Client/src/components/oneSpot.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import axios from 'axios';
 import MapContainer from './map.jsx';
 import SpotMap from './spotMap.jsx';
+import { connect } from "react-redux";
+import { render } from "react-dom";
+import { addSpotToCart } from "../actions/cart.js";
 
+@connect((store) => {
+  return {
+    cart: store.cart
+  }
+})
 
 class OneSpot extends React.Component {
   constructor(props) {
@@ -24,6 +32,11 @@ class OneSpot extends React.Component {
       .catch(function (error) {
         console.log(error);
       });
+  }
+
+  addToCart(e) {
+    console.log('add to cart bind this e =', e.target.value);
+    this.props.dispatch(addSpotToCart(JSON.parse(e.target.value)));
   }
 
 
@@ -50,6 +63,15 @@ class OneSpot extends React.Component {
                       <p className="title is-4">{spot.spotName}</p>
                       <br></br>
                       <p className="subtitle is-6">{spot.description}</p>
+                      <div className="control">
+                        <button
+                          className="button is-primary"
+                          type="submit"
+                          value={JSON.stringify(spot)}
+                          onClick={this.addToCart.bind(this)} >
+                          Add to my Trip!
+                        </button>
+                      </div>
                       <div class="modal">
                         <div class="modal-background"></div>
                         <div class="modal-content">
