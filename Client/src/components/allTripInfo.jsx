e0ff59a4c1ecfe488202d156c0bcf6a19dad6f12
Nick Chapman <nickpchapman@gmail.com>
2017-12-19 21:53:39 -0800

    Add new header for allTrips

--- Client/src/components/allTripInfo.jsx
+++ Client/src/components/allTripInfo.jsx
@@ -4,21 +4,32 @@ import OneSpot from './oneSpot.jsx';
 
 let AllTripInfo = (props) => (
   <div>
-    <div className="card">
-      <div className="">
-        <img src={props.location.query.thumbnail} alt="Placeholder image"/>
-      </div>
-      <div className="card-content">
-        <div className="media">
-          <div className="media-content">
-            <p className="title is-4">{props.location.query.tripName}</p>
-            <br></br>
-            <p className="subtitle is-6">{props.location.query.description}</p>
-          </div>
+    <div className="container">
+      <div className="columns">
+        <div className="column">
+          <p className="title">{props.location.query.tripName}</p>
+          <p className="subtitle">{props.location.query.destination}</p>
+          <hr/>
+          <p className="subtitle">{props.location.query.description}</p>
+          <hr/>
+          <p className="subtitle">Created by {props.location.query.username}</p>
+          <hr/>
+          <p className="subtitle is-size-6">{props.location.query.spots.length + ' spots on this trip'}</p>
+          <p className="subtitle is-size-6">{props.location.query.hashtag}</p>
+          <hr/>
+          <a className="button is-primary">Add All Spots From This Trip</a>
+        </div>
+
+        <div className="column">
+          <figure className="image">
+            <img src={props.location.query.thumbnail} alt="Placeholder image"/>
+          </figure>
         </div>
       </div>
     </div>
+
     <h3 class="title is-3">All Spots on This Trip:</h3>
+
 
     <OneSpot trip={props}/>
 
