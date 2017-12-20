07b1fb093e6b27b9b39c5a8bf850caf867cfdc63
Nick Chapman <nickpchapman@gmail.com>
2017-12-20 14:45:10 -0800

    Changed class to className for allTripInfo

--- Client/src/components/buildTrip.jsx
+++ Client/src/components/buildTrip.jsx
@@ -51,7 +51,7 @@ class BuildTrip extends React.Component {
           <p className="title">Build a Trip!</p>
           <form >
             <div className="field">
-              <label class="label">Trip Name</label>
+              <label className="label">Trip Name</label>
               <p className="control">
                 <input
                   className="input"
@@ -64,7 +64,7 @@ class BuildTrip extends React.Component {
               </p>
             </div>
             <div className="field">
-              <label class="label">Destination</label>
+              <label className="label">Destination</label>
               <div className="control">
                 <input
                   className="input"
@@ -77,7 +77,7 @@ class BuildTrip extends React.Component {
               </div>
             </div>
             <div className="field">
-            <label class="label">Trip Description</label>
+            <label className="label">Trip Description</label>
               <p className="control">
                 <textarea
                   className="textarea"
@@ -90,7 +90,7 @@ class BuildTrip extends React.Component {
               </p>
             </div>
             <div className="field">
-              <label class="label">Add Hash Tags</label>
+              <label className="label">Add Hash Tags</label>
               <p className="control">
                 <input
                   className="input"
@@ -134,8 +134,7 @@ class BuildTrip extends React.Component {
             </figure>
           </div>
         </div>
-      </div>
-    </div>
+
   </div>
   )
   }
