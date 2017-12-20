e494156642bff99d0ff1db9156acb39a7dce163e
Nick Chapman <nickpchapman@gmail.com>
2017-12-20 15:56:19 -0800

    Rename build trip to upload trip. styled header for allTripInfo

--- Client/src/components/allTripInfo.jsx
+++ Client/src/components/allTripInfo.jsx
@@ -7,19 +7,16 @@ let AllTripInfo = (props) => (
     <div className="container">
       <div className="columns">
         <div className="column">
-          <p className="title">{props.location.query.tripName}</p>
-          <p className="subtitle">{props.location.query.destination}</p>
+          <p className="title is-size-2">{props.location.query.tripName}</p>
+          <p className="subtitle is-size-5">{props.location.query.destination}</p>
           <hr/>
-          <p className="subtitle">{props.location.query.description}</p>
-          <hr/>
-          <p className="subtitle">Created by {props.location.query.username}</p>
-          <hr/>
-          <p className="subtitle is-size-6">{props.location.query.spots.length + ' spots on this trip'}</p>
+          <p className="subtitle is-size-3">{props.location.query.description}</p>
+          <p className="subtitle is-size-6">Created by {props.location.query.username}</p>
           <p className="subtitle is-size-6">{props.location.query.hashtag}</p>
           <hr/>
-          <a className="button is-primary">Add All Spots From This Trip</a>
+          <p className="subtitle is-size-6">{props.location.query.spots.length + ' spots on this trip'}</p>
+          <hr/>
         </div>
-
         <div className="column">
           <figure className="image">
             <img src={props.location.query.thumbnail} alt="Placeholder image"/>
@@ -27,9 +24,16 @@ let AllTripInfo = (props) => (
         </div>
       </div>
     </div>
-
-    <h3 class="title is-3">All Spots on This Trip:</h3>
-
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            All Spots on This Trip:
+          </h1>
+          <a className="button is-primary">Add all spots</a>
+        </div>
+      </div>
+    </section>
 
     <OneSpot trip={props}/>
 
