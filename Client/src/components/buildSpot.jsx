e6200e31becb1c6c26a382a72bb7d0982a4875b9
Phillip Jun <29644671+waffiltime@users.noreply.github.com>
2017-12-21 11:54:16 -0800

    Add browser geolocation if no image metadata

--- Client/src/components/buildSpot.jsx
+++ Client/src/components/buildSpot.jsx
@@ -17,10 +17,15 @@ import { NavLink } from 'react-router-dom';
 class BuildSpot extends React.Component {
 
   getUserCoords() {
-    return navigator.geolocation.getCurrentPosition((position) => {
-      let pos = {'lat': position.coords.latitude, 'lng': position.coords.longitude};
-      console.log('pos =', pos);
-      return pos;
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let pos = {'lat': position.coords.latitude, 'lng': position.coords.longitude};
+        console.log('pos =', pos);
+        if(!pos) {
+          reject('get user coords promise error');
+        }
+        resolve(pos);
+      });
     });
   }
 
@@ -56,17 +61,18 @@ class BuildSpot extends React.Component {
         });
         let info = {..._this.props.cloudinaryStore.tempSpot};
         if(!result[0].image_metadata.GPSLatitude) {
-          let userGPS = window.navigator.geolocation.getCurrentPosition((position) => {
-            let pos = {'lat': position.coords.latitude, 'lng': position.coords.longitude};
-            console.log('pos =', pos);
-            return pos;
+          _this.getUserCoords().then((userGPS)=> {
+            info.lat = userGPS.lat;
+            info.long = userGPS.lng;
+            console.log('no image loc! here is user GPS loc', userGPS);
+            console.log('no image loc! here is user info', info);
+            _this.props.dispatch(cloudinarySpotInfo(info));
+            _this.props.dispatch(cloudinaryGallery(url));
+          }).catch((error) => {
+            if(error) {
+              console.log('.catch error =', error);
+            }
           });
-          info.lat = userGPS.lat;
-          info.long = userGPS.lng;
-          console.log('no image loc! here is user GPS loc', userGPS);
-          console.log('no image loc! here is user info', info);
-          _this.props.dispatch(cloudinarySpotInfo(info));
-          _this.props.dispatch(cloudinaryGallery(url));
         } else {
           info.lat = _this.convertToDecimal(result[0].image_metadata.GPSLatitude);
           info.long = _this.convertToDecimal(result[0].image_metadata.GPSLongitude);
