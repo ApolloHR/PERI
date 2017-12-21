211df72bb65aa5dd0a668b9a00e2d10ac03adce5
Nick Chapman <nickpchapman@gmail.com>
2017-12-20 16:05:09 -0800

    change buildTrip to uploadTrip in nav and routes, updated filename

--- Client/src/components/UploadTrip.jsx
+++ Client/src/components/UploadTrip.jsx
@@ -11,7 +11,7 @@ import { Link } from 'react-router-dom';
   }
 })
 
-class BuildTrip extends React.Component {
+class UploadTrip extends React.Component {
 
   uploadWidget() {
     let _this = this;
@@ -139,4 +139,4 @@ class BuildTrip extends React.Component {
   )
   }
 }
-export default BuildTrip;
+export default UploadTrip;
--- Client/src/components/routes.jsx
+++ Client/src/components/routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Nav from './nav.jsx';
 import Landing from './landing.jsx';
-import BuildTrip from './buildTrip.jsx';
+import UploadTrip from './uploadTrip.jsx';
 import Cart from './cart.jsx';
 import TripViewer from './tripViewer.jsx';
 import AllTripInfo from './allTripInfo.jsx';
@@ -17,7 +17,7 @@ let Routes = (props) => (
     <div>
       <Nav />
       <Route exact path="/" component={Landing}/>
-      <Route path="/buildTrip" component={BuildTrip}/>
+      <Route path="/uploadTrip" component={UploadTrip}/>
       <Route path="/buildSpot" component={BuildSpot}/>
       <Route path="/cart" component={Cart}/>
       <Route path="/tripViewer" component={TripViewer}/>
