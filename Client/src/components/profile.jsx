683dc588942344c85d21deb84bdbca47d28671d5
Benji <thebkrcanbefound@gmail.com>
2018-01-02 12:25:35 -0800

    Starts to render specific user trips

--- Client/src/components/profile.jsx
+++ Client/src/components/profile.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import Axios from 'axios';
+import axios from 'axios';
+import HitComponent from './hitComponent.jsx';
 
 class Profile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userToSearch: ''
+      userToSearch: '',
+      profileFound: false,
+      userProfile: {}
     };
 
     this.submitUserSearch = this.submitUserSearch.bind(this);
@@ -19,38 +22,89 @@ class Profile extends React.Component {
   }
 
   submitUserSearch() {
+    var context = this;
+    var data = this.state.userToSearch;
 
+    axios.post('/getProfile', {data})
+      .then((res) => {
+        console.log('WHATtt it actually worked??!! res ==', res);
+        context.setState({
+          userProfile: res,
+          profileFound: true
+        });
+      })
+      .catch((err) => {
+        console.log('error GETting profile line 37 profile.jsx');
+      });
   }
 
 
   render() {
-    return (
-      <div>
-        Profile
-        <div class="field">
-          <div className="control is-expanded">
-            <label class="label">Search User:</label>
-            <input
-              className="input"
-              type="text"
-              name="description"
-              placeholder="Enter Place Description"
-              onChange={this.getUserData}
-              required
-            />
+    var context = this;
+    if (!context.state.profileFound) {
+      return (
+        <div>
+          Profile
+          <div class="field">
+            <div className="control is-expanded">
+              <label class="label">Search User:</label>
+              <input
+                className="input"
+                type="text"
+                name="description"
+                placeholder="Username"
+                onChange={this.getUserData}
+                required
+              />
+            </div>
+          </div>
+          <div className="control">
+            <button
+              className="button is-primary"
+              type="submit"
+              value="submit"
+              onClick={this.submitUserSearch} >
+              Find!
+            </button>
           </div>
         </div>
-        <div className="control">
-          <button
-            className="button is-primary"
-            type="submit"
-            value="submit"
-            onClick={this.submitUserSearch} >
-            Find!
-          </button>
+      );
+    } else {
+      return (
+        <div>
+          Profile
+          <div class="field">
+            <div className="control is-expanded">
+              <label class="label">Search User:</label>
+              <input
+                className="input"
+                type="text"
+                name="description"
+                placeholder="Username"
+                onChange={this.getUserData}
+                required
+              />
+            </div>
+          </div>
+          <div className="control">
+            <button
+              className="button is-primary"
+              type="submit"
+              value="submit"
+              onClick={this.submitUserSearch} >
+              Find!
+            </button>
+          </div>
+
+          {context.state.userProfile.map((trip, i) => {
+            return (
+              <HitComponent trip={trip} key={i} />
+            );
+          })}
         </div>
-      </div>
-    );
+      );
+    }
+
   }
 }
 
