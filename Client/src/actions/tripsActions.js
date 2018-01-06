1d534cc1eab199a58390b5fef53c0f2916a1a564
Phillip Jun <29644671+waffiltime@users.noreply.github.com>
2018-01-05 16:32:57 -0800

    Code comment cleanup

--- Client/src/actions/tripsActions.js
+++ Client/src/actions/tripsActions.js
@@ -31,19 +31,3 @@ export function postTrip(data) {
 export function login(value) {
   return { type: 'LOG_IN', payload: value };
 }
-// {type: "FETCH_TRIPS_SUCCESS", payload:[{
-//     username: 'nick',
-//     tripName: 'London 2017',
-//     destination: 'London',
-//     description: 'Lads trip to UK 2017',
-//     thumbnail: 'https://media.timeout.com/images/103042848/image.jpg',
-//     spots: [{spotID: 1}]
-//   },
-//   {
-//     username: 'larry',
-//     tripName: 'Brazil 2017',
-//     destination: 'Brazil',
-//     description: 'Lads trip to Brazil 2017',
-//     thumbnail: 'http://knowledge.wharton.upenn.edu/wp-content/uploads/2016/03/rio.jpg',
-//     spots: [{spotID: 2}]
-//   }]}
